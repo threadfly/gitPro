7831cfa40ce8fcf9f00e14a57c8f0b6b9f49e108
threadfly <294815470@qq.com>
2015-09-21 10:10:07 +0800

    ./build_protobuf.sh modify

--- project/bin/build_protobuf.sh
+++ project/bin/build_protobuf.sh
@@ -1,3 +1,6 @@
 #!/bin/sh 
-
+MESSAGEDIR="../protocol/protobuf/message/"
+if [ ! -d $MESSAGEDIR ];then
+	mkdir -p $MESSAGEDIR
+fi
 protoc --proto_path=../protocol/protobuf/proto  --cpp_out=../protocol/protobuf/message ../protocol/protobuf/proto/*
